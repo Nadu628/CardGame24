d7bc47df363ea4099e3a928794d99253bf82e916
Nadu628 <eugen5@farmingdale.edu>
2025-03-09 19:09:24 -0400

    added other features, still cant get to display images

--- src/main/java/org/example/cardgame24/CardGameController.java
+++ src/main/java/org/example/cardgame24/CardGameController.java
@@ -8,8 +8,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.InputStream;
+import java.net.URL;
 import java.util.*;
-import java.util.zip.ZipFile;
+
 
 public class CardGameController {
     private Map<String, Integer> cardValues;
@@ -18,6 +19,9 @@ public class CardGameController {
 
     @FXML
     private Label enterAnExpressionLabel;
+
+    @FXML
+    private Label resultLabel;
 
     @FXML
     private TextField enteredExpressionTextField;
@@ -48,33 +52,43 @@ public class CardGameController {
         for (int i = 2; i <= 10 ; i++) {
             cardValues.put(String.valueOf(i),i);
         }
-        cardValues.put("Ace", 1);
-        cardValues.put("Jack", 11);
-        cardValues.put("Queen", 12);
-        cardValues.put("King", 13);
+        cardValues.put("ace", 1);
+        cardValues.put("jack", 11);
+        cardValues.put("queen", 12);
+        cardValues.put("king", 13);
+
+
     }
 
     public void initialize() {
+        System.out.println("initalize");
+        currentCardValues= new ArrayList<>();
         refreshButton.setOnAction(e -> {
             refreshCards();
         });
+
+        verifyButton.setOnAction(e -> {
+            verifyExpression();
+        });
+
+        refreshCards();
     }
 
-    public List<Image> loadImageFromZip(String zipFilePath) throws Exception{
-        List<Image> images = new ArrayList<>();
+    private List<String> createDeck(){
+        List<String> deck = new ArrayList<>();
+        String[] suits = {"hearts", "diamonds", "clubs", "spades"};
+        String[] faceCards = {"ace", "jack", "queen", "king"};
 
-        try(ZipFile zipFile = new ZipFile(zipFilePath)){
-            zipFile.stream().filter(zipEntry -> zipEntry.getName().endsWith(".png"))
-                        .forEach(zipEntry -> {
-                            try(InputStream stream = zipFile.getInputStream(zipEntry)){
-                                images.add(new Image(stream));
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        });
+        //add numberedCards
+        for(String suit : suits){
+            for(int i = 2; i <= 10; i++){
+                deck.add(i + "_of_" + suit + ".png");
             }
-
-            return images;
+            for(String faceCard : faceCards){
+                deck.add(faceCard + "_of_" + suit + ".png");
+            }
+        }
+        return deck;
     }
 
     private List<String> getRandomDeck(){
@@ -83,53 +97,112 @@ public class CardGameController {
         return deck.subList(0,4); //get 4 cards
     }
 
-    private List<String> createDeck(){
-        List<String> deck = new ArrayList<>();
-        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
-        String[] faceCards = {"Ace", "Jack", "Queen", "King"};
-
-        //add numberedCards
-        for(String suit : suits){
-            for(int i = 2; i <= 10; i++){
-                deck.add(i + " of " + suit);
-            }
-            for(String faceCard : faceCards){
-                deck.add(faceCard + " of " + suit);
-            }
-        }
-        return deck;
-    }
-
     public int getCardValue(String cardName) {
-        return cardValues.getOrDefault(cardName, 0);
+        return cardValues.getOrDefault(cardName.split("_")[0], 0);
     }
 
-    private void refreshCards(){
+    @FXML
+    private void refreshCards() {
         currentCardNames = getRandomDeck();
-        currentCardValues = new ArrayList<>();
+        currentCardValues.clear();
 
-        for(int i = 0; i < currentCardNames.size(); i++){
-            String cardName = currentCardNames.get(i);
-            int cardValue = getCardValue(cardName.split(" ")[0]);
-            currentCardValues.add(cardValue);
+        displayCards();
+    }
+    private void displayCards(){
+        ImageView[] imageViews = {card1ImageView, card2ImageView, card3ImageView, card4ImageView};
 
-            String imagePath = "/cards/." + cardName.replace(" ", "_") + ".png";
-            Image cardImage = new Image(getClass().getResourceAsStream(imagePath));
+        for (int i = 0; i < currentCardNames.size(); i++) {
+            String imageFileName = currentCardNames.get(i);
+            InputStream imageStream = getClass().getClassLoader().getResourceAsStream("org/example/cardgame24/cards/" + imageFileName);
+            if(imageStream != null){
+                Image cardImage = new Image(imageStream);
+                imageViews[i].setImage(cardImage);
+            }else{
+                System.out.println("Image not found");
+            }
 
-            if(i == 0){
-                card1ImageView.setImage(cardImage);
-            }
-            if(i == 1){
-                card2ImageView.setImage(cardImage);
-            }
-            if(i == 2){
-                card3ImageView.setImage(cardImage);
-            }
-            if(i == 3){
-                card4ImageView.setImage(cardImage);
+        }
+    }
+
+    private boolean isValidExpression(String expression) {
+        String[] tokens = expression.split(" ");
+        Set<Integer> usedCards = new HashSet<>();
+
+        for (String token : tokens) {
+            if(cardValues.containsKey(token)){
+                usedCards.add(cardValues.get(token));
             }
         }
+        return usedCards.size() == 4 && usedCards.containsAll(new HashSet<>(currentCardValues));
+    }
 
+    private double evaluateExpression(String expression) throws Exception {
+        expression = expression.replace(" ", "");
+        double result = 0;
+        String[] parts = expression.split("[-+*/]");
+
+        result = Double.parseDouble(parts[0]);
+
+        for(int i = 1; i < parts.length; i++){
+            char operator = expression.charAt(parts[i-1].length());
+            double nextNum = Double.parseDouble(parts[i]);
+
+            switch(operator){
+                case '+':
+                    result += nextNum;
+                    break;
+                case '-':
+                    result -= nextNum;
+                    break;
+                case '*':
+                    result *= nextNum;
+                    break;
+                case '/':
+                    if(nextNum == 0){
+                        result /= nextNum;
+                    }else{
+                        throw new ArithmeticException("Division by zero");
+                    }
+                    break;
+
+            }
+        }
+        return result;
+    }
+
+    private void verifyExpression() {
+        String expression = enteredExpressionTextField.getText().trim();
+
+        try{
+            if(isValidExpression(expression)){
+                double result = evaluateExpression(expression);
+                if(result == 24){
+                    resultLabel.setText("Correct result: " + result);
+                }else{
+                    resultLabel.setText("Incorrect result: " + result + ". Try again.");
+                }
+            }else{
+                resultLabel.setText("Invalid expression. Try again.");
+            }
+        }catch(Exception e){
+            resultLabel.setText("Invalid expression. Try again.");
+        }
+    }
+
+
+
+
+
+
+    public void testImageAccess() {
+        String testImage = "cards/10_of_spades.png";
+        URL testUrl = getClass().getClassLoader().getResource(testImage);
+
+        if (testUrl == null) {
+            System.out.println("Test Image NOT FOUND: " + testImage);
+        } else {
+            System.out.println("Test Image FOUND at: " + testUrl);
+        }
     }
 
 
